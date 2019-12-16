68f94c72e7e21488b6468c2ca8c4598a25af8e84
cameron-charles <cameron.ntc@gmail.com>
2019-12-15 18:33:47 -0700

    added post topics to post component

--- src/components/post.js
+++ src/components/post.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 
 class Post extends Component {
+  renderTopics() {
+    let topics = this.props.associated_topics.map((topic, index) => {
+      return (
+        <span className="post-topic" key={index}>
+          {topic}
+        </span>
+      );
+    });
+    return topics;
+  }
   render() {
-    return <li key="index">{this.props.post.title}</li>;
+    return (
+      <li className="recent-post">
+        <div className="recent-post-title">{this.props.title}</div>
+        <div className="recent-post-topics">{this.renderTopics()}</div>
+      </li>
+    );
   }
 }
 
